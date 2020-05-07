9dc41ad378d1872775c5f5b471b309d851b4f741
Rdadaiya Krinish <47221638+krinish291@users.noreply.github.com>
2020-05-07 17:15:32 +0530

    tic tac toe

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,38 +1,10 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
-
-
-import Header from './components/Header';
-import Home from './components/Home'
-import CSVUpload from './components/CSVUpload'
-
-import './components/style.css';
+import React from 'react';
+import './App.css';
+import Game from './components/Game';
 
 function App() {
- 
   return (
-    <div>
-        <Header />
-        <Switch>
-        {/* If the current URL is /about, this route is rendered
-            while the rest are ignored */}
-        <Route path="/home">
-          <Home />
-        </Route>
-        
-        <Route path="/csvupload">
-          <CSVUpload />
-        </Route>
-        <Route path="/">
-          <Home />
-        </Route>
-      </Switch>
-      </div>
+   <Game />
   );
 }
 
